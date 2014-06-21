a62785638c8a35931282600ec77bb0dd7493e128
Justin Thiele <justin.thiele@gmail.com>
2014-06-21 15:02:03 -0700

    Additional email notifications for webhooks

--- app/controllers/koudoku/webhooks_controller.rb
+++ app/controllers/koudoku/webhooks_controller.rb
@@ -9,27 +9,34 @@ module Koudoku
       data_json = JSON.parse request.body.read
     
       if data_json['type'] == "invoice.payment_succeeded"
-        
         stripe_id = data_json['data']['object']['customer']
         amount = data_json['data']['object']['total'].to_f / 100.0
         subscription = ::Subscription.find_by_stripe_id(stripe_id)
         subscription.payment_succeeded(amount)
+
       elsif data_json['type'] == "charge.failed"
-    
         stripe_id = data_json['data']['object']['customer']
-      
-        # subscription = ::Subscription.find_by_stripe_id(stripe_id)
-        subscription = ::Subscription.last
+        subscription = ::Subscription.find_by_stripe_id(stripe_id)
         subscription.charge_failed
         ::Notifier.reportedly_payment_failed(subscription)
+        ::Notifier.user_payment_failed(subscription)
     
       elsif data_json['type'] == "charge.dispute.created"
-    
         stripe_id = data_json['data']['object']['customer']
-    
         subscription = ::Subscription.find_by_stripe_id(stripe_id)
         subscription.charge_disputed
+        ::Notifier.reportedly_charge_disputed(subscription)
       
+      elsif data_json['type'] == "customer.subscription.deleted"
+        stripe_id = data_json['data']['object']['customer']
+        subscription = ::Subscription.find_by_stripe_id(stripe_id)
+        ::Notifier.reportedly_subscription_cancelled(subscription)
+
+      elsif data_json['type'] == "customer.subscription.trial_will_end"
+        stripe_id = data_json['data']['object']['customer']
+        subscription = ::Subscription.find_by_stripe_id(stripe_id)
+        ::Notifier.reportedly_trial_ending(subscription)
+
       end
       
       render nothing: true
